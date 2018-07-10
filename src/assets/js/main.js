ac54b42c0f54ea6874762cc16e4e4c7a93e229a8
MatheoDev404 <stunaultmatheo.dev@gmail.com>
2018-07-10 09:16:32 +0200

    ajustement couleurs/nav 10-07-2018

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -41,7 +41,6 @@ $(()=>{
             anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
             if ( anchor.length ) {
                 $("html, body").animate( { scrollTop: anchor.offset().top }, 1000);
-                
             }
         }
     });
